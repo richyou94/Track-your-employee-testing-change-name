b60afc7b7315318255b1757f3e3a452f6a39a4c2
RichardBass You <richardbassyou@RichardBasssMBP.lan>
2022-10-06 14:35:41 -0700

    added index.js Model for connecting the primary key and foreign key

--- models/index.js
+++ models/index.js
@@ -2,3 +2,31 @@ const Department = require('./Department');
 const Role = require('./Role');
 const Employee = require('./Employee');
 
+Department.hasOne(Role, {
+    foreignKey: 'department_id',
+    onDelete: 'CASCADE',
+});
+
+Role.belongsTo(Department, {
+    foreignKey: 'department_id',
+});
+
+Role.hasOne(Employee, {
+    foreignKey: 'role_id',
+    onDelete: 'CASCADE',
+});
+
+Employee.belongsTo(Role, {
+    foreignKey: 'role_id',
+});
+
+Employee.hasOne(Employee, {
+    foreignKey: 'manager_id',
+    onDelete: 'CASCADE',
+});
+
+Employee.belongsTo(Employee, {
+    foreignKey: 'manager_id',
+});
+
+module.exports = {Department, Role, Employee};
