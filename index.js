f95f65283efae714450183f684ce43ee0ba75034
RichardBass You <richardbassyou@RichardBasssMBP.lan>
2022-10-11 12:03:02 -0700

    almost done with employees view, manager name is not selected yet

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function initOption() {
 
 function viewEmployee() {
     console.log(`-----------Employee List-----------`) 
-    const sql = 'SELECT employee.id AS id, employee.name AS employee FROM department';
+    const sql = 'SELECT employee.id AS id, employee.first_name AS first_name, employee.last_name AS last_name, role.title AS title, department.name AS department, role.salary AS salary, employee.manager_id AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id';
     db.query(sql, (err, rows) => {
         console.table(rows);
         initOption();
